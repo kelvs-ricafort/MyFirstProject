9da955c03f42ffc623f9b6607a75f964b308d9e7
Kelvin Ricafort <kelvs.r@outlook.com>
2023-10-14 13:39:05 +0800

    Snackbar Messages

--- app/src/main/java/com/newtechieblog/wordpress/views/myfirstproject/MainActivity.java
+++ app/src/main/java/com/newtechieblog/wordpress/views/myfirstproject/MainActivity.java
@@ -2,31 +2,20 @@ package com.newtechieblog.wordpress.views.myfirstproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
-import android.widget.ToggleButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
     Button ok;
     TextView result;
     ImageView image;
-
-
+    LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,11 +25,19 @@ public class MainActivity extends AppCompatActivity {
         ok = findViewById(R.id.btnOK);
         result = findViewById(R.id.ResultTextView);
         image = findViewById(R.id.imageExample);
+        linearLayout = findViewById(R.id.linear);
 
         ok.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "This is a Toast Message", Toast.LENGTH_LONG).show();
+                // Toast.makeText(getApplicationContext(), "This is a Toast Message", Toast.LENGTH_LONG).show();
+
+                Snackbar.make(linearLayout, "This is a Snackbar message", Snackbar.LENGTH_INDEFINITE).setAction("Close", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                }).show();
             }
         });
     }
