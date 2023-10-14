4bcc4f89b2a0847eb036d92c2d0ccd88dd943007
Kelvin Ricafort <kelvs.r@outlook.com>
2023-10-14 14:36:32 +0800

    ListView

--- app/src/main/java/com/newtechieblog/wordpress/views/myfirstproject/MainActivity.java
+++ app/src/main/java/com/newtechieblog/wordpress/views/myfirstproject/MainActivity.java
@@ -1,7 +1,9 @@
 package com.newtechieblog.wordpress.views.myfirstproject;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
     Button ok;
     TextView result;
     ImageView image;
-    LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,20 +26,41 @@ public class MainActivity extends AppCompatActivity {
         ok = findViewById(R.id.btnOK);
         result = findViewById(R.id.ResultTextView);
         image = findViewById(R.id.imageExample);
-        linearLayout = findViewById(R.id.linear);
+
 
         ok.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Toast.makeText(getApplicationContext(), "This is a Toast Message", Toast.LENGTH_LONG).show();
 
-                Snackbar.make(linearLayout, "This is a Snackbar message", Snackbar.LENGTH_INDEFINITE).setAction("Close", new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
+//                Snackbar.make(linearLayout, "This is a Snackbar message", Snackbar.LENGTH_INDEFINITE).setAction("Close", new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//
+//                    }
+//                }).show();
 
-                    }
-                }).show();
+                showDialogMessage();
             }
         });
+    }
+
+    private void showDialogMessage() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setTitle("Delete")
+                .setMessage("Do you want to delete text?")
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                })
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        result.setText("");
+                    }
+                }).show();
+        alertDialog.create();
     }
 }
